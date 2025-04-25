a03598caa64516dcc1b6258d88004b049823052b
iramos0301 <167824949+iramos0301@users.noreply.github.com>
2025-04-25 09:40:05 -1000

    Linked bulletin to news and announcements page in Waipahu High School Website

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ class Footer extends HTMLElement {
     <nav class="fixed-bottom bg-dark">
         <ul class="nav nav-justified align-items-center">
             <a class="nav-link col-2" href="../Navigation/quickAccess.html"><i class="bi bi-hand-index-fill"></i></a>
-            <a class="nav-link col-2" href="../Navigation/Bulletin.html"><i class="bi bi-clipboard-fill"></i></a>
+            <a class="nav-link col-2" href="https://www.waipahuhigh.org/apps/news/index.jsp?id=0"><i class="bi bi-clipboard-fill"></i></a>
             <a class="nav-link col-3" href="../index.html"><button type="button"
                 class="btn text-warning bg-primary rounded-circle btn-lg" style="width: 75px; height: 75px;"><i
                     class="bi bi-house h2"></i></button></a>
